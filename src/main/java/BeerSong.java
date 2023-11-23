fc0846c37011f238a156f9987336e7433c147b38
Alexandr Varenyshev <alex.varenyshev@gmail.com>
2023-11-23 21:47:58 +0300

    finished beer song

---
+++ src/main/java/BeerSong.java
@@ -0,0 +1,19 @@
+public class BeerSong {
+    public static void  main (String[] args) {
+        int beerNum = 99;
+        String word = "бутылки";
+        while (beerNum>0) {
+            System.out.println(beerNum +" " +word + " пива на стене" );
+            System.out.println("Возьми одну.");
+            System.out.println("Пусти по кругу");
+            beerNum = beerNum - 1;
+            if (beerNum>0) {
+                if (beerNum==1) {
+                    word = "бутылка"; //ед.ч
+                }
+            } else {
+                System.out.println("Нет бутылок пива на стене");
+            }// конец еще
+        }//конец while
+    }//конец main
+}//конец класса
